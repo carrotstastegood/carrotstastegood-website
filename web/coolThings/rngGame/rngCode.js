df187e0d94fd430df1e85a23377f31051f5ededa
carrotstastegood <22matti.kivinen@gmail.com>
2025-09-30 09:07:05 -0400

    Logic fixes

--- web/coolThings/rngGame/rngCode.js
+++ web/coolThings/rngGame/rngCode.js
@@ -1,7 +1,7 @@
 // Coded this to learn. Doesn't take a while when you can already code.
-let answer = Math.floor((Math.random() * 50) + 1);
+const answer = Math.floor((Math.random() * 50) + 1);
 let turns = 5;
-let guess = ""
+let guess = "";
 
 alert("Welcome to my number guessing game! It is my first javascript project as I try to learn the language.");
 
@@ -14,29 +14,28 @@ while (turns >= 1) {
 
     if (guess < answer) { // Player's guess is too low
         console.log("Too low:", guess);
-        alert(`Too low! ${turns} attempts left.`);
         turns -= 1;
+        alert(`Too low! ${turns} attempts left.`);
 
     } else if (guess > answer) { // Player's guess is too high
-        console.log("Too high:" + guess);
-        alert(`Too high! ${turns} attempts left!`);
+        console.log("Too high: " + guess);
         turns -= 1;
-
-    } else if (guess == answer) { // Player guesses correct
-        let attempts = 5 - turns;
+        alert(`Too high! ${turns} attempts left!`);
+        
+    } else if (guess === answer) { // Player guesses correct
         console.log("Correct:", guess)
-        console.log(`You found the answer ${answer} in ${5 - turns} attempts.`);
-        alert(`You found the answer ${answer} in ${5 - turns} attempts.`);
+        console.log(`You found the answer ${answer} in ${5 - turns} attempt(s).`);
+        alert(`You found the answer ${answer} in ${5 - turns} attempt(s).`);
         break;
 
     } else { // User provides garbage
-        console.log("Invalid input:" + guess);
+        console.log("Invalid input: " + guess);
         alert("You must enter a number. No turns lost.");
     };
     
 };
 
 if (turns == 0) {
-    console.log("You ran out of turns, the answer was" + answer);
-    alert("You ran out of turns, the answer was" + answer + ".");
+    console.log("You ran out of turns, the answer was " + answer);
+    alert("You ran out of turns, the answer was " + answer + ".");
 };
